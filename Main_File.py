25722704f91aef2f9c981f21cc1c7f5f730d9aa8
Akash <akash.darknight@gmail.com>
2017-10-18 14:05:00 +0530

    Application of RNN

--- Main_File.py
+++ Main_File.py
@@ -1,3 +1,10 @@
 import numpy as np
 from utils import *
 import random
+
+data = open('dinos.txt', 'r').read()
+data= data.lower()
+chars = list(set(data))
+print(sorted(chars))
+data_size, vocab_size = len(data), len(chars)
+print('There are %d total characters and %d unique characters in your data.' % (data_size, vocab_size))
