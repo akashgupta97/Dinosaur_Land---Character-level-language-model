26a356dd0f336a6a0f257066908050ac58c0c62c
Akash <akash.darknight@gmail.com>
2017-10-19 14:05:00 +0530

    Application of RNN

--- Main_File.py
+++ Main_File.py
@@ -8,3 +8,9 @@ chars = list(set(data))
 print(sorted(chars))
 data_size, vocab_size = len(data), len(chars)
 print('There are %d total characters and %d unique characters in your data.' % (data_size, vocab_size))
+
+
+char_to_ix = { ch:i for i,ch in enumerate(sorted(chars)) }
+ix_to_char = { i:ch for i,ch in enumerate(sorted(chars)) }
+print(ix_to_char)
+
