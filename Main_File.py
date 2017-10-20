edd8c5802ef41698870e3ef71cfc34480cb89f3b
Akash <akash.darknight@gmail.com>
2017-10-20 14:05:00 +0530

    Application of RNN

--- Main_File.py
+++ Main_File.py
@@ -14,3 +14,17 @@ char_to_ix = { ch:i for i,ch in enumerate(sorted(chars)) }
 ix_to_char = { i:ch for i,ch in enumerate(sorted(chars)) }
 print(ix_to_char)
 
+
+### GRADED FUNCTION: clip
+
+def clip(gradients, maxValue):
+    '''
+    Clips the gradients' values between minimum and maximum.
+
+    Arguments:
+    gradients -- a dictionary containing the gradients "dWaa", "dWax", "dWya", "db", "dby"
+    maxValue -- everything above this number is set to this number, and everything less than -maxValue is set to -maxValue
+
+    Returns:
+    gradients -- a dictionary with the clipped gradients.
+    '''
