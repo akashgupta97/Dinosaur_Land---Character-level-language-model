7c6449a1d187a765b582cc5ad707708aa3901c54
Akash <akash.darknight@gmail.com>
2017-10-21 14:05:00 +0530

    Application of RNN

--- Main_File.py
+++ Main_File.py
@@ -28,3 +28,16 @@ def clip(gradients, maxValue):
     Returns:
     gradients -- a dictionary with the clipped gradients.
     '''
+
+
+dWaa, dWax, dWya, db, dby = gradients['dWaa'], gradients['dWax'], gradients['dWya'], gradients['db'], gradients['dby']
+
+### START CODE HERE ###
+# clip to mitigate exploding gradients, loop over [dWax, dWaa, dWya, db, dby]. (≈2 lines)
+for gradient in [dWax, dWaa, dWya, db, dby]:
+    np.clip(gradient, -maxValue, maxValue, out=gradient)
+### END CODE HERE ###
+
+gradients = {"dWaa": dWaa, "dWax": dWax, "dWya": dWya, "db": db, "dby": dby}
+
+return gradients
