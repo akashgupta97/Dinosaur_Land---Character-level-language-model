9eb4d17978f2676f847ac4418cdbc8770881b539
Akash <akash.darknight@gmail.com>
2017-11-18 14:05:00 +0530

    Application of RNN

--- Main_File.py
+++ Main_File.py
@@ -142,3 +142,33 @@ indices = sample(parameters, char_to_ix, 0)
 print("Sampling:")
 print("list of sampled indices:", indices, '\n')
 print("list of sampled characters:", [ix_to_char[i] for i in indices])
+
+
+# GRADED FUNCTION: optimize
+
+def optimize(X, Y, a_prev, parameters, learning_rate=0.01):
+    """
+    Execute one step of the optimization to train the model.
+
+    Arguments:
+    X -- list of integers, where each integer is a number that maps to a character in the vocabulary.
+    Y -- list of integers, exactly the same as X but shifted one index to the left.
+    a_prev -- previous hidden state.
+    parameters -- python dictionary containing:
+                        Wax -- Weight matrix multiplying the input, numpy array of shape (n_a, n_x)
+                        Waa -- Weight matrix multiplying the hidden state, numpy array of shape (n_a, n_a)
+                        Wya -- Weight matrix relating the hidden-state to the output, numpy array of shape (n_y, n_a)
+                        b --  Bias, numpy array of shape (n_a, 1)
+                        by -- Bias relating the hidden-state to the output, numpy array of shape (n_y, 1)
+    learning_rate -- learning rate for the model.
+
+    Returns:
+    loss -- value of the loss function (cross-entropy)
+    gradients -- python dictionary containing:
+                        dWax -- Gradients of input-to-hidden weights, of shape (n_a, n_x)
+                        dWaa -- Gradients of hidden-to-hidden weights, of shape (n_a, n_a)
+                        dWya -- Gradients of hidden-to-output weights, of shape (n_y, n_a)
+                        db -- Gradients of bias vector, of shape (n_a, 1)
+                        dby -- Gradients of output bias vector, of shape (n_y, 1)
+    a[len(X)-1] -- the last hidden state, of shape (n_a, 1)
+    """
